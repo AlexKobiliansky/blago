5cb297832d12740acde91fc5ac941b2fd5cbb698
alex <alex_kobilianskiy@bigmir.net>
2020-10-10 20:28:14 +0300

    index.html complete

--- app/js/common.js
+++ app/js/common.js
@@ -96,6 +96,22 @@ $(document).ready(function(){
         heightses();
     });
 
+    $('.preloader').fadeOut();
+
+    $(function() {
+        $("a[href='#popup-form']").magnificPopup({
+            type: "inline",
+            fixedContentPos: !1,
+            fixedBgPos: !0,
+            overflowY: "auto",
+            closeBtnInside: !0,
+            preloader: !1,
+            midClick: !0,
+            removalDelay: 300,
+            mainClass: "my-mfp-zoom-in"
+        })
+    });
+
     $('.products-slider').owlCarousel({
         loop: true,
         nav: true,
@@ -140,15 +156,21 @@ $(document).ready(function(){
     $('input[type="checkbox"]').styler();
 
     //E-mail Ajax Send
-    $("form").submit(function() { //Change
+    $(".contact-form").submit(function() { //Change
         var th = $(this);
+        var t = th.find(".btn").text();
+        th.find(".btn").prop("disabled", "disabled").addClass("disabled").text("Отправлено!");
 
         $.ajax({
             type: "POST",
             url: "mail.php", //Change
             data: th.serialize()
         }).done(function() {
-
+            setTimeout(function() {
+                th.find(".btn").removeAttr('disabled').removeClass("disabled").text(t);
+                th.trigger("reset");
+                $.magnificPopup.close();
+            }, 2000);
         });
         return false;
     });
